853f61a5f2158f4a62df95080477f690710d8ff6
unknown <daisuke.asaoka@MW7IGBAUZNGQYN.dir.svc.accenture.com>
2015-10-19 15:29:19 +0900

    Basic view now returns minimal HTML

--- functional_test.py
+++ functional_test.py
@@ -13,8 +13,8 @@ class NewVisitorTest(unittest.TestCase):    #
     def test_can_start_a_list_and_retrieve_it_later(self):  #
         #Edith has heard about a cool new online to-do app.
         #She goes to check out its homepage
-        self.browser.get('http://localhost:8000')
-
+        #self.browser.get('http://localhost:8000') #for mac
+        self.browser.get('http://localhost:8080') # for windows
         #She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)  #
         self.fail('Finish the test!')
